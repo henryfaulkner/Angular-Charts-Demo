9185e380136827d6a76bacec869787929516c74b
henryfaulkner-now <henry.faulkner@nowcorp.com>
2023-09-29 09:00:29 -0400

    colors

---
+++ src/app/core/ui-helpers/color-constants.ts
@@ -0,0 +1,9 @@
+export class ColorConstants {
+  static SoftWhite = '#ebebeb';
+  static White = '#fff';
+  static NowOrange = '#ff8700';
+  static NowGreenPrimary = '#244538';
+  static NowGreenSecondary = '#c1d2c7';
+  static PlaceholderGrey = '#cfcfcf';
+  static KendoThemeRed = '#f31700';
+}
